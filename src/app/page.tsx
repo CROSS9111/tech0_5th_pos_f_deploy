574ee7b9b72bccd368359233086e7eac240d5f93
CROSS9111 <hirotoshikurosu@gmail.com>
2024-02-19 02:06:59 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,187 @@
-import Image from "next/image";
+'use client'
+import React from 'react';
+import { useState,useEffect } from 'react';
+import BarcodeReader from './components/BarcodeReader';
+import ProductDisplay from './components/ProductDisplay';
+import PurchaseList from './components/PurchaseList';
+import TitleBar from './components/TitleBar';
+// import Quagga from "quagga";
+
+// import styles from '../styles/Home.module.css'; // Next.js推奨のCSSモジュールを使用する
+
+type Product = {
+  PRD_ID: number;
+  PRD_CD: string;
+  PRD_NAME: string;
+  PRD_PRICE: number;
+};
+
 
 export default function Home() {
+  const [product, setProduct] = useState<Product | null>(null);
+  const [items, setItems] = useState<Product[]>([]);
+  const [showScanner, setShowScanner] = useState<boolean>(false);
+  const [animateTriangle, setAnimateTriangle] = useState<boolean>(false);
+  const [searchCode, setSearchCode] = useState<string>("");
+
+  
+  const handleScan = async (code) => {
+    console.log("handleScan Start")
+    // console.log(code)
+
+    setShowScanner(false); // スキャンが完了したらバーコードリーダーを非表示にする
+    const res = await fetch(
+      "https://tech0-gen-5-step4-webapp-kurotest.azurewebsites.net/search_product/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: code }),
+    });
+
+    const data = await res.json();
+    console.log(data)
+    // ここは暫定      
+    if (data.status === "success") {
+        setProduct(data.message);
+        console.log(data.message)
+      } else {
+        setProduct(null);
+      }
+  };
+
+  const handleSearch = () => {
+    //入ってなかった場合
+    if (!searchCode) {
+      alert("コードが入力されていません");
+      return;
+    }
+
+    handleScan(searchCode);
+  };
+
+  const handleAdd = () => {
+    //入ってなかった場合
+    if (!product || !product.PRD_NAME) {
+      alert("スキャン商品が不正です");
+      return;
+    }
+
+    setAnimateTriangle(true); // アニメーションを開始
+
+    setTimeout(() => {
+      setAnimateTriangle(false); // アニメーションをリセット
+    }, 200); // CSSアニメーションの持続時間
+    // itemsに入れる
+
+    setItems([...items, product]);
+    setProduct(null);
+  };
+
+  const handlePurchase = async () => {
+    if (items.length === 0) {
+      alert("商品が追加されていません");
+      return; // ここで関数を終了し、APIコールをスキップ
+    }
+
+    const purchaseData = {
+      EMP_CD: "9999999999",
+      STORE_CD: "00030",
+      POS_NO: "090",
+      items: items,
+    };
+
+    try {
+      const response = await fetch(
+          "http://127.0.0.1:8000/purchase/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(purchaseData),
+        }
+      );
+
+      const responseData = await response.json();
+      console.log(responseData)
+
+      // APIから帰ってきたメッセージをポップアップで表示
+      if (responseData.message) {
+        alert(JSON.stringify(responseData.message, null, 2));
+      }
+
+      setItems([]); // itemsを空にします
+    } catch (error) {
+      console.error("Error making a purchase:", error);
+    }
+  };
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <TitleBar />
+      <div className="flex flex-col items-center justify-start min-h-screen bg-white">
+        {/* showScannerがtrueならReaderを表示する */}
+        {showScanner ? (
+            <BarcodeReader onScan={handleScan} />
+        ) : (
+        <div className="w-full max-w-md p-4">
+              {/* スキャンボタン */}
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full mb-4"
+                onClick={() => setShowScanner(true)}
+              >
+                スキャン
+              </button>
+              
+              {/* ORテキスト */}
+              <p className="text-center my-2">OR</p>
+
+              {/* コード入力エリア */}
+              <div className="flex items-center border-b border-blue-500 py-2">
+                <input
+                  type="text"
+                  value={searchCode}
+                  className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                  onChange={(e) => setSearchCode(e.target.value)}
+                  placeholder="コードを入力"
+                />
+              </div>
+
+              {/* 検索ボタン */}
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full mt-4"
+                onClick={handleSearch}
+              >
+                検索
+              </button>
+            
+
+              {/* プロダクトの名前表示エリア */}
+              <ProductDisplay product={product} />
+
+              {/* 検索ボタン */}
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full w-full mb-4"
+               onClick={handleAdd}>
+                追加
+              </button>
+
+              <div
+                className={`triangle-down ${animateTriangle ? "animate" : ""}`}
+              ></div>
+              <div className="text-purchase">購入リスト</div>
+              <PurchaseList items={items} />
+              <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full w-full mb-4" onClick={handlePurchase}>
+                購入
+              </button>
         </div>
+          
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+
