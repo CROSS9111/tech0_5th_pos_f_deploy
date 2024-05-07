551d8ecb0bd2c807c84a411e391cf32b9c6536d8
CROSS9111 <hirotoshikurosu@gmail.com>
2024-05-07 22:59:23 +0900

    commit

---
+++ __tests__/sample.test.jsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Page from '../src/app/page';
+
+describe('Page Component', () => {
+  it('初期から「いいね」というテキストが表示されている', () => {
+    render(<Page />);
+
+    // 「いいね」というテキストが初期から表示されていることを確認
+    expect(screen.getByText('スキャン')).toBeInTheDocument();
+  });
+});
