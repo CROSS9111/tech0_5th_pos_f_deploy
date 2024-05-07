e20e9ed4d3277715d51a342e1ac1c6960f48772d
CROSS9111 <hirotoshikurosu@gmail.com>
2024-05-08 08:50:22 +0900

    commit

--- __tests__/sample.test.jsx
+++ __tests__/sample.test.jsx
@@ -4,10 +4,10 @@ import { render, screen } from '@testing-library/react';
 import Page from '../src/app/page';
 
 describe('Page Component', () => {
-  it('初期から「***」というテキストが表示されている', () => {
+  it('特定（Tech0 STEP4 POSアプリ）というテキストが表示されている', () => {
     render(<Page />);
 
     // 「いいね」というテキストが初期から表示されていることを確認
-    expect(screen.getByText('スキャン')).toBeInTheDocument();
+    expect(screen.getByText('Tech0 STEP4 POSアプリ')).toBeInTheDocument();
   });
 });
