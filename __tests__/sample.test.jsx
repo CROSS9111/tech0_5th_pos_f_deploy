9e075104b7697f531565ada0549b80fb2e0d4607
CROSS9111 <hirotoshikurosu@gmail.com>
2024-05-08 10:37:39 +0900

    title change POS -> お疲れ様

--- __tests__/sample.test.jsx
+++ __tests__/sample.test.jsx
@@ -7,16 +7,13 @@ describe('Page Component', () => {
   it('特定（Tech0 STEP4 POSアプリ）というテキストが表示されている', () => {
     render(<Page />);
 
-    // 「いいね」というテキストが初期から表示されていることを確認
     expect(screen.getByText('Tech0 STEP4 POSアプリ')).toBeInTheDocument();
   });
 });
 
 // describe('Page Component', () => {
-//   it('「kurosu」というテキストがページ上に存在しない', () => {
+//   it('「Tech0 STEP4 POSアプリ」というテキストがページ上に存在しない', () => {
 //     render(<Page />);
-
-//     // 「POS」というテキストがページ上にないことを確認
 //     expect(screen.queryByText('Tech0 STEP4 POSアプリ')).toBeNull();
 //   });
 // });
@@ -24,8 +21,6 @@ describe('Page Component', () => {
 // describe('Page Component', () => {
 //   it('特定（最終発表お疲れ様でした！）というテキストが表示されている', () => {
 //     render(<Page />);
-
-//     // 「いいね」というテキストが初期から表示されていることを確認
 //     expect(screen.getByText(最終発表お疲れ様でした！')).toBeInTheDocument();
 //   });
 // });
