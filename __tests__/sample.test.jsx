6955c5c5e7c7001329da42a3fb358ff1d9dad84e
CROSS9111 <hirotoshikurosu@gmail.com>
2024-05-08 10:52:33 +0900

    title change POS -> お疲れ様 testcode modify

--- __tests__/sample.test.jsx
+++ __tests__/sample.test.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Page from '../src/app/page';
 
-describe('Page Component', () => {
-  it('特定（Tech0 STEP4 POSアプリ）というテキストが表示されている', () => {
-    render(<Page />);
+// describe('Page Component', () => {
+//   it('特定（Tech0 STEP4 POSアプリ）というテキストが表示されている', () => {
+//     render(<Page />);
 
-    expect(screen.getByText('Tech0 STEP4 POSアプリ')).toBeInTheDocument();
+//     expect(screen.getByText('Tech0 STEP4 POSアプリ')).toBeInTheDocument();
+//   });
+// });
+
+describe('Page Component', () => {
+  it('「Tech0 STEP4 POSアプリ」というテキストがページ上に存在しない', () => {
+    render(<Page />);
+    expect(screen.queryByText('Tech0 STEP4 POSアプリ')).toBeNull();
   });
 });
 
-// describe('Page Component', () => {
-//   it('「Tech0 STEP4 POSアプリ」というテキストがページ上に存在しない', () => {
-//     render(<Page />);
-//     expect(screen.queryByText('Tech0 STEP4 POSアプリ')).toBeNull();
-//   });
-// });
-
-// describe('Page Component', () => {
-//   it('特定（最終発表お疲れ様でした！）というテキストが表示されている', () => {
-//     render(<Page />);
-//     expect(screen.getByText(最終発表お疲れ様でした！')).toBeInTheDocument();
-//   });
-// });
+describe('Page Component', () => {
+  it('特定（最終発表お疲れ様でした！）というテキストが表示されている', () => {
+    render(<Page />);
+    expect(screen.getByText('最終発表お疲れ様でした！')).toBeInTheDocument();
+  });
+});
